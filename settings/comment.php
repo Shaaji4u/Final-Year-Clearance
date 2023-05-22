857f31674bc3a041e1a39498082a690b9876e5f7
Amedari4 <58507339+Amedari4@users.noreply.github.com>
2023-05-22 11:12:18 +0100

    Add files via upload

---
+++ settings/comment.php
@@ -0,0 +1,18 @@
+<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+	<blockquote>
+		 <p>The system rocks!!!. delay in final clearance has been eliminated.</p>
+		 <small>by <cite>Jude Okonkwo.</cite></small>
+	</blockquote>
+</div>
+<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+	<blockquote>
+		 <p>This a welcome development, one needs not travel back to school just for final year clearance process. Thanks to the school management for this initiative.</p>
+		 <small>by <cite>Sadiya AbdulGafar.</cite></small>
+	</blockquote>
+</div>
+<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+	<blockquote>
+		 <p>I love this. i hope to see more ICT initiatives from the management soon.</p>
+		 <small>by <cite>Maryam Sherif A.</cite></small>
+	</blockquote>
+</div>
---
+++ settings/footer_file.php
@@ -0,0 +1,8 @@
+<?php
+
+?>
+<div class="row clearfix label-primary hidden-print" style="margin-top:0px;">
+	<footer style="margin-top:15px">
+		<h4 style="text-align:center;color:white;">Copyright &copy; 2022 - All Rights Reserved - Jamila Idris - CST/17/SWE/00027 | 08143400694 | fashionswizz@gmail.com.</h4>
+	</footer>
+</div>
